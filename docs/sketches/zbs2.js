93434d35eaef685fcfd35201add6c44864126b95
agaldanaw <38634379+agaldanaw@users.noreply.github.com>
2021-07-17 00:53:16 +0000

    Deploying to gh-pages from  @ 9517949cede1930b653a797232edf972c4cdc760 🚀

--- docs/sketches/zbs2.js
+++ docs/sketches/zbs2.js
@@ -3,7 +3,7 @@ let img; // Declarar variable 'img'.
 
 function setup() {
   createCanvas(350, 150);
-  img = loadImage('/vc/docs/sketches/zbs2.png'); // Cargar la imagen
+  img = loadImage('/vc/docs/sketches/zbs2.PNG'); // Cargar la imagen
 }
 
 function draw() {
