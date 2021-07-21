f03544c36300efbcb197acb2c1fffefac6aab490
Ozai98 <alagarciape@unal.edu.co>
2021-07-21 02:14:53 +0000

    Deploying to gh-pages from  @ 27767c70b502fcdc8eaedd4d24c1b84fc4a99235 🚀

--- docs/sketches/depthmap.js
+++ docs/sketches/depthmap.js
@@ -5,7 +5,7 @@ var near, far;
 var easycam;
 
 function preload() {
-  depthShader = loadShader('/hugo-vc/sketches/z-shader.vert', '/hugo-vc/sketches/z-depthmap.frag');
+  depthShader = loadShader('/vc/docs/sketches/z-shader.vert', '/vc/docs/sketches/z-depthmap.frag');
 }
 
 function setup () {  
