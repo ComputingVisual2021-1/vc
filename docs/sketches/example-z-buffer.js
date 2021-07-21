a6fed2433f6f879650a45491aef641d34a335f9d
Andres Garcia <alagarciape@unal.edu.co>
2021-07-20 20:30:55 -0500

    a

--- docs/sketches/example-z-buffer.js
+++ docs/sketches/example-z-buffer.js
@@ -2,10 +2,10 @@
 let img; // Declarar variable 'img'.
 
 function setup() {
-  createCanvas(781, 603);
+  createCanvas(603, 781);
   img = loadImage('/vc/docs/sketches/example_z_buffer.png'); // Cargar la imagen
 }
 
 function draw() {
-  image(img, 0, 0, 781, 603);
+  image(img, 0, 0, 603, 781);
 }
