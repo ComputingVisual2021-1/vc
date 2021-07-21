0ec6ed33c6c2cc9d796cbe9d8a98069ca16c50eb
Ozai98 <alagarciape@unal.edu.co>
2021-07-21 01:28:11 +0000

    Deploying to gh-pages from  @ 16d25b77b5a275c7e06fa72d8e6e7eb7750e36ed 🚀

--- docs/sketches/example-z-buffer.js
+++ docs/sketches/example-z-buffer.js
@@ -2,10 +2,10 @@
 let img; // Declarar variable 'img'.
 
 function setup() {
-  createCanvas(650, 560);
+  createCanvas(781, 603);
   img = loadImage('/vc/docs/sketches/example_z_buffer.png'); // Cargar la imagen
 }
 
 function draw() {
-  image(img, 0, 0, 650, 560);
+  image(img, 0, 0, 781, 603);
 }
