2ddd297184d1da66982b363eb233dd9213681664
Camilo Andrés Carranza <ccarranzac@slabcode.com>
2021-07-26 02:03:39 -0500

    mosaic fixes

--- docs/sketches/mosaic-hardware.js
+++ docs/sketches/mosaic-hardware.js
@@ -1,55 +1,88 @@
-let W;
-let H;
-
-let slider;
-
-let myShader;
-
+let image;
+let om;
+let mosaic;
+let resolution;
+let om_on;
 
 function preload() {
-    img = loadImage('/vc/docs/sketches/main.jpg');
+    //Precargar imagen base y shaders
+    image = loadImage('/vc/docs/sketches/main.jpg');
+    om = loadImage('/vc/docs/sketches/img13.png');
+    mosaic = loadShader('/vc/docs/sketches/mosaic.vert', '/vc/docs/sketches/mosaic.frag');
 
-    alpha0 = loadImage('/vc/docs/sketches/img1.jpg')
-    alpha1 = loadImage('/vc/docs/sketches/img2.jpg')
-    alpha2 = loadImage('/vc/docs/sketches/img3.jpg')
-    alpha3 = loadImage('/vc/docs/sketches/img4.jpg')
-    alpha4 = loadImage('/vc/docs/sketches/img5.jpg')
-    alpha5 = loadImage('/vc/docs/sketches/img6.jpg')
-    alpha6 = loadImage('/vc/docs/sketches/img6.jpg')
-
-    myShader = loadShader("/vc/docs/sketches/moasic.vert", "/vc/docs/sketches/mosaic.frag")
+    //Cargar imagenes de dataset para mosaico
+    img0 = loadImage('/vc/docs/sketches/img0.jpg');
+    img1 = loadImage('/vc/docs/sketches/img1.jpg');
+    img2 = loadImage('/vc/docs/sketches/img2.jpg');
+    img3 = loadImage('/vc/docs/sketches/img3.jpg');
+    img4 = loadImage('/vc/docs/sketches/img4.jpg');
+    img5 = loadImage('/vc/docs/sketches/img5.jpg');
+    img6 = loadImage('/vc/docs/sketches/img6.jpg');
+    img7 = loadImage('/vc/docs/sketches/img7.jpg');
+    img8 = loadImage('/vc/docs/sketches/img8.jpg');
+    img9 = loadImage('/vc/docs/sketches/img9.jpg');
+    img10 = loadImage('/vc/docs/sketches/img10.jpg');
+    img11 = loadImage('/vc/docs/sketches/img11.jpg');
+    img12 = loadImage('/vc/docs/sketches/img12.jpg');
+    
+    
 }
 
 function setup() {
-    W = 500;
-    H = 500;
-    createCanvas(W, H, WEBGL);
+    //Crear canvas
+    createCanvas(600, 600, WEBGL);
     textureMode(NORMAL);
     noStroke();
-    shader(myShader);
-    myShader.setUniform("texture", img);
-    myShader.setUniform("alpha", alpha);
-    myShader.setUniform("alpha0", alpha0);
-    myShader.setUniform("alpha1", alpha1);
-    myShader.setUniform("alpha2", alpha2);
-    myShader.setUniform("alpha3", alpha3);
-    myShader.setUniform("alpha4", alpha4);
-    myShader.setUniform("alpha5", alpha5);
-    myShader.setUniform("alpha6", alpha6);
-
-    slider = createSlider(2, 16, 40);
-    slider.position(10, 10);
+    //definir los shaders a usar
+    shader(mosaic);
+    mosaic.setUniform('om', om);
+    //Mostrar en pantalla una casilla de verificación para intercambiar entre el mosaico y la imagen original
+    om_on = createCheckbox('Mosaic', false);
+    om_on.changed(() => mosaic.setUniform('om_on', om_on.checked()));
+    om_on.position(10, 10);
+    //Pasar imagenes a fragment shader
+    mosaic.setUniform('img', image);
+    mosaic.setUniform('img0', img0);
+    mosaic.setUniform('img1', img1);
+    mosaic.setUniform('img2', img2);
+    mosaic.setUniform('img3', img3);
+    mosaic.setUniform('img4', img4);
+    mosaic.setUniform('img5', img5);
+    mosaic.setUniform('img6', img6);
+    mosaic.setUniform('img7', img7);
+    mosaic.setUniform('img8', img8);
+    mosaic.setUniform('img9', img9);
+    mosaic.setUniform('img10', img10);
+    mosaic.setUniform('img11', img11);
+    mosaic.setUniform('img12', img12);
+    
+    //Mostrar en pantalla un deslizador para ajustar la resolución de la imagen y el mosaico a mostrar
+    resolution = createSlider(10, 100, 30, 1);
+    resolution.position(10, 50);
+    resolution.style('width', '80px');
+    resolution.input(() => mosaic.setUniform('resolution', resolution.value()));
+    mosaic.setUniform('resolution', resolution.value());
 }
 
 function draw() {
+    background(33);
+    
+    cover(true);
+}
 
-    let posSlider = slider.value();
-    myShader.setUniform("resolution", parseInt(500 / posSlider));
-
+function cover(texture = false) {
     beginShape();
-    vertex(-W / 2, -H / 2, 0, 0, 0);
-    vertex(W / 2, -H / 2, 0, 1, 0);
-    vertex(W / 2, H / 2, 0, 1, 1);
-    vertex(-W / 2, H / 2, 0, 0, 1);
-    endShape();
+    if (texture) {
+        vertex(-width / 2, -height / 2, 0, 0, 0);
+        vertex(width / 2, -height / 2, 0, 1, 0);
+        vertex(width / 2, height / 2, 0, 1, 1);
+        vertex(-width / 2, height / 2, 0, 0, 1);
+    }
+    else {
+        vertex(-width / 2, -height / 2, 0);
+        vertex(width / 2, -height / 2, 0);
+        vertex(width / 2, height / 2, 0);
+        vertex(-width / 2, height / 2, 0);
+    }
+    endShape(CLOSE);
 }
