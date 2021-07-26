390a7bafb9f416a09023ac3f0291a95ec91827cc
Ozai98 <alagarciape@unal.edu.co>
2021-07-26 09:03:51 +0000

    Deploying to gh-pages from  @ 5c0d0d8b49bc11c89c2fdd61d86678292cad09f3 🚀

---
+++ docs/sketches/image-to-ascii-hardware.js
@@ -0,0 +1,52 @@
+let image;
+let ascii;
+
+function preload() {
+    //Precargar imagen base y shaders
+    image = loadImage('/vc/docs/sketches/medieval.jpg');
+    ascii = loadShader('/vc/docs/sketches/ascii.vert', '/vc/docs/sketches/ascii.frag');
+
+    //Cargar imagenes de dataset para ascii
+    img1 = loadImage('/vc/docs/sketches/99.jpg'); //nivel de brillo mas bajo
+    img2 = loadImage('/vc/docs/sketches/88.jpg');
+    img3 = loadImage('/vc/docs/sketches/77.jpg');
+    img4 = loadImage('/vc/docs/sketches/66.jpg');
+    img5 = loadImage('/vc/docs/sketches/55.jpg');
+    img6 = loadImage('/vc/docs/sketches/44.jpg');
+    img7 = loadImage('/vc/docs/sketches/33.jpg');
+    img8 = loadImage('/vc/docs/sketches/22.jpg');
+    img9 = loadImage('/vc/docs/sketches/11.jpg');
+    img10 = loadImage('/vc/docs/sketches/00.jpg'); //nivel de brillo mas alto    
+}
+
+function setup() {
+    //Crear canvas
+    createCanvas(1600, 1600, WEBGL);
+    textureMode(NORMAL);
+    noStroke();
+    //definir los shaders a usar
+    shader(ascii);
+    //Pasar imagenes a fragment shader
+    ascii.setUniform('img', image);
+    ascii.setUniform('img1', img1);
+    ascii.setUniform('img2', img2);
+    ascii.setUniform('img3', img3);
+    ascii.setUniform('img4', img4);
+    ascii.setUniform('img5', img5);
+    ascii.setUniform('img6', img6);
+    ascii.setUniform('img7', img7);
+    ascii.setUniform('img8', img8);
+    ascii.setUniform('img9', img9);
+    ascii.setUniform('img10', img10);
+
+}
+
+function draw() {    
+    ascii.setUniform("res", parseInt(1600/2));
+    beginShape();
+    vertex(-1600 / 2, -1600 / 2, 0, 0, 0);
+    vertex(1600 / 2, -1600 / 2, 0, 1, 0);
+    vertex(1600 / 2, 1600 / 2, 0, 1, 1);
+    vertex(-1600 / 2, 1600 / 2, 0, 0, 1);
+    endShape();
+}
